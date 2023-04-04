545073baa1d55ed079830fb1772f0c362f4ff6ca
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-04-03 21:33:20 -0500

    added employee prompt index.js

--- index.js
+++ index.js
@@ -8,3 +8,40 @@ const { Engineer, engineerQuestionsArr } = require('./lib/Engineer');
 const { Intern, internQuestionsArr } = require('./lib/Intern')
 
 const employeesArr = []
+
+const init = () => { managerQuestions() } 
+
+const managerQuestions = () => {
+    inquirer.createPromptModule(managerQuestionsArr)
+    .then(( answers ) => {
+        answers = new Manager(answers.name, answers.email, answers.officeNumber)
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
+const engineerQuestions = () => {
+    inquirer.createPromptModule(engineerQuestionsArr)
+    .then(( answers ) => {
+        answers = new Engineer(answers.name, answers.id, answers.github)
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
+const internQuestions = () => {
+    inquirer.createPromptModule(internQuestionsArr)
+    .then(( answers ) => {
+        answers = new Engineer(answers.name, answers.id, answers.github)
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
+const employeePrompt = () => {
+    inquirer.createPromptModule([{
+        type: 'list',
+        name: 'employeeType',
+        message: "What type"
+    }])
+}
